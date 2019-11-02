30c8bd5759de7959c076c8d8f72ab669533e9c07
Claudio Garaycochea <computadoraweb@gmail.com>
2019-11-01 23:58:05 -0300

    Dashboard

--- src/components/dashboard/core/sideMenu/SideMenu.jsx
+++ src/components/dashboard/core/sideMenu/SideMenu.jsx
@@ -15,44 +15,40 @@ class SideMenu extends Component {
 				<div className='side-menu-header position-relative'>
 					<div className='logo-wrapper'>
 						<i className='logo-public-bloc' />
-						<div className='text'>Public Bloc</div>
+						<div className='text'>Weband.tv</div>
 					</div>
 				</div>
 				<div className='menu-items'>
 					<Link to='/dashboard' className='text-white'>
 						<div className='item'>
 							<i className='icon-home-white small space' />
-							{'Home'}
+							INTERACTIVITY
 						</div>
 					</Link>
 					<div className='item'>
 						<i className='icon-financial-white small space' />
-						{'Financial Dashboard'}
+						MICROPAYMENTS
 					</div>
 					<div className='item'>
 						<i className='icon-bell-white small space' />
-						{'Notifications'}
+						BIG DATA
 					</div>
 					<div className='item'>
 						<i className='icon-invoice-white small space' />
-						{'Invoices'}
+						ANALISYS
 					</div>
 					<div className='item'>
 						<i className='icon-contractor-white small space' />
-						{'Contractors'}
+						SUPPORT
 					</div>
 					<div className='item'>
 						<i className='icon-calendar-white small space' />
-						{'Calendar'}
-					</div>
-					<div className='item'>
-						<i className='icon-settings-white small space' />
-						{'Settings'}
+						INNOVATION
 					</div>
 				</div>
 				<div className='sidebar_footer'>
 					<span className='text-medium'>
-						{`Copyright &copy; ${new Date().getFullYear()} Public Bloc`}
+						{`Copyright ${new Date().getFullYear()} Weband.tv`}
 					</span>
 				</div>
 			</div>
